ea60439148e6d8bfc85d8688733efff2125c9786
noninho18 <elion.hashani@telecomnancy.net>
2023-11-22 09:48:40 +0100

    no tensor

--- python/core/lyrics_generator.py
+++ python/core/lyrics_generator.py
@@ -1,10 +1,5 @@
 import numpy as np 
 import pandas as pd
 
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, LSTM
-from keras.utils import np_utils
-from keras.callbacks import ModelCheckpoint
-
 def oui():
     print("oui")
