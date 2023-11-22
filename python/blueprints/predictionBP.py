895fe6abc51dfba391ec75f722b480ac72596b09
Bourdais Rémi <rm.bourdais@gamil.com>
2023-11-22 18:11:50 +0100

    Prediction page

--- python/blueprints/predictionBP.py
+++ python/blueprints/predictionBP.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,session
+from flask import Blueprint, request
 from flask.templating import render_template
 from python.core.prediction import predict_popularity
 
@@ -8,10 +8,23 @@ predictionBP = Blueprint('predictionBP', __name__)
 # Definition of the main route
 @predictionBP.route("/prediction")
 def prediction():
-    #get data from the form     
-    fake_data = {"lyrics": "I'm a little teapot, short and stout. Here is my handle, here is my spout. When I get all steamed up, hear me shout. Tip me over and pour me out!", "genre": "Pop", "artist_name": "Ariana Grande", "track_name": "tea pot", "tempo": 120, "clé": 0 , "explicit" : True, "duration" : 200}
-    score = predict_popularity(fake_data)
-    print(score)
     genres = ['blues','country','hip hop','jazz','pop','reggae','rock']
     notes = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
-    return render_template("prediction.html", genre=genres, note=notes, score=score)
+    return render_template("prediction.html", genre=genres, note=notes)
+
+@predictionBP.route("/get-result", methods=['POST'])    
+def get_result():  
+    notes = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
+    
+    paroles = request.form['paroles']
+    genre = request.form['genre']
+    artist_name = request.form['artist_name']
+    track_name = request.form['track_name']
+    tempo = int(request.form['tempo'])
+    key = notes.index(request.form['key'])
+    explicit = int(request.form['explicit'])
+    duration = int(request.form['duration'])*1000
+    data = {"lyrics": paroles, "genre": genre, "artist_name": artist_name, "track_name": track_name, "tempo": tempo, "cle": key , "explicit" : explicit, "duration" : duration}
+    
+    score = predict_popularity(data)
+    return render_template("result-pred.html",score=score)
